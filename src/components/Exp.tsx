0d6953a65f752013ef9fd18fb5b7a8f9c7ab3574
goutham4331 <goutham4331@gmail.com>
2024-06-07 14:22:04 -0500

    rm build errors

--- src/components/Exp.tsx
+++ src/components/Exp.tsx
@@ -10,7 +10,7 @@ const Exp = () => {
       <p>Nov 2020 - Sep 2021 · 11 mosNov 2020 to Sep 2021 · 11 mos</p>
       <p>Bangalore Urban, Karnataka, India · On-siteBangalore Urban, Karnataka, India · On-site</p>
       <ul>
-        <li>Contributed to the conversion of the application's UI to a new, optimized design, resulting in improved user experience.</li>
+        <li>{`Contributed to the conversion of the applications UI to a new, optimized design, resulting in improved user experience.`}</li>
         <li>Overhauled legacy code to recent React code while adhering to best coding practices, resulting in increased maintainability.</li>
         <li>Implemented 3 forms dependent on API calls and stored their information and selections in Redux.</li>
         <li>Created a white-label version of the application for two external clients.</li>
